70ab63ca07dcc3474043de27f0fe1fc9baf12251
JustANormalThing <yagyaev.s.i.22@gmail.com>
2026-09-22 23:59:46 +0300

    Initial commit

---
+++ Dino.c
@@ -0,0 +1,138 @@
+#define SDL_MAIN_HANDLED
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <SDL3/SDL.h>
+#include <SDL3_image/SDL_image.h>
+
+#define NUM_CHARACTERS 18
+
+typedef struct {
+    const char *name;
+    const char *png;
+} Character;
+
+int main(void)
+{
+
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        printf("SDL_Init Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    Character chars[NUM_CHARACTERS] = {
+        {"Reimu Hakurei","re.png"},
+        {"Marisa Kirisame","Ma.png"},
+        {"Rumia","Ru.png"},
+        {"Daiyousei","3pixels_char1.png"},
+        {"Cirno", "9.png"},
+        {"Hong Melling","Mel.png"},
+        {"Koakuma","SecondOne.png"},
+        {"Patchouli Knowledge","Book.png"},
+        {"Sakuya Izayoi", "Knife.png"},
+        {"Remillia Scarlet","Remi.png"},
+        {"Flandre Scarlet","TasyFoodName.png"},
+        {"Letty Whiterock","TasyFoodName.png"},
+        {"Chen","Cat.png"},
+        {"Alice Margatroid","Aris.png"},
+        {"Lily White",          "Happy.png"},
+        {"Lunasa Prismriver","Prism.png"},
+        {"Merlin Prismriver","Prism2.png"},
+        {"Lyrica Prismriver","Lyrica.png"}
+
+    };
+
+    srand((unsigned int)time(NULL));
+
+    int index = rand() % NUM_CHARACTERS;
+
+    printf("Selected character: %s\n", chars[index].name);
+    printf("Loading image: %s\n", chars[index].png);
+
+
+    SDL_Window *win = SDL_CreateWindow(
+        "Touhou Character",
+        800,
+        800,
+        0
+    );
+
+    if (win == NULL) {
+        printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer *ren = SDL_CreateRenderer(win, NULL);
+
+    if (ren == NULL) {
+        printf("SDL_CreateRenderer Error: %s\n", SDL_GetError());
+
+        SDL_DestroyWindow(win);
+        SDL_Quit();
+
+        return 1;
+    }
+
+    SDL_Surface *surface = IMG_Load(chars[index].png);
+
+    if (surface == NULL) {
+        printf("IMG_Load Error: %s\n", SDL_GetError());
+
+        SDL_DestroyRenderer(ren);
+        SDL_DestroyWindow(win);
+        SDL_Quit();
+
+        return 1;
+    }
+
+
+    SDL_Texture *tex = SDL_CreateTextureFromSurface(ren, surface);
+
+    SDL_DestroySurface(surface);
+
+    if (tex == NULL) {
+        printf(
+            "SDL_CreateTextureFromSurface Error: %s\n",
+            SDL_GetError()
+        );
+
+        SDL_DestroyRenderer(ren);
+        SDL_DestroyWindow(win);
+        SDL_Quit();
+
+        return 1;
+    }
+
+
+    int quit = 0;
+    SDL_Event e;
+
+    while (!quit) {
+
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_EVENT_QUIT) {
+                quit = 1;
+            }
+        }
+
+
+        SDL_SetRenderDrawColor(ren, 255, 255, 255, 255);
+        SDL_RenderClear(ren);
+
+
+        SDL_RenderTexture(ren, tex, NULL, NULL);
+
+
+        SDL_RenderPresent(ren);
+    }
+
+
+    SDL_DestroyTexture(tex);
+    SDL_DestroyRenderer(ren);
+    SDL_DestroyWindow(win);
+
+    SDL_Quit();
+
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,30 @@
+## About project
+This project generates a random touhou chars, with there png.
+
+## Geting started
+
+You need SDL3 and SDL_Img using mingw for this project, I dont know if other work for this
+Here are links for them:
+
+https://github.com/libsdl-org/SDL_image/releases/tag/release-3.4.6
+
+https://github.com/libsdl-org/SDL/releases
+
+Latest version up to this date 22.09.26
+To compile this project
+```
+gcc Dino.c -o Dino.exe -I"C:\Users\Admin\Desktop\NEWTEST\SMALL_PROJECT\SDL3-3.4.16\x86_64-w64-mingw32\include" -I"C:\Users\Admin\Desktop\NEWTEST\SMALL_PROJECT\SDL3_image-3.4.6\x86_64-w64-mingw32\include" -L"C:\Users\Admin\Desktop\NEWTEST\SMALL_PROJECT\SDL3-3.4.16\x86_64-w64-mingw32\lib" -L"C:\Users\Admin\Desktop\NEWTEST\SMALL_PROJECT\SDL3_image-3.4.6\x86_64-w64-mingw32\lib" -lmingw32 -lSDL3 -lSDL3_image 
+```
+This shoud be in one line.
+
+## Credit
+Art by:
+Koakuma by hanahello
+Daiyousei (Daiyōsei) - Touhou - Image by NIMA
+
+Big credit for SDL for making this possible
+https://github.com/libsdl-org/SDL
+https://github.com/libsdl-org/SDL_image/tree/main
+
+Made using Gcc compiler.
+Using mingw.
